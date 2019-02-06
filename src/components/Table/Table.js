9a9e161e1abcf103fcdd4ba470ce7d7d5968dec3
Vojta Oupicky <voj.oup@gmail.com>
2019-02-06 23:47:32 +0100

    Enable adding new records

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -11,55 +11,101 @@ export default class Table extends Component {
       err: '',
     };
     this.renderWeightRecords = this.renderWeightRecords.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
     this.renderWeightRecords();
   }
 
-  renderWeightRecords() {
-    fetch('/.netlify/functions/records')
-      .then(response => response.json())
-      .then(records => {
-        if (!records.error) {
-          this.setState({ data: records });
+  async handleSubmit(e) {
+    e.preventDefault();
+
+    const weightValue = this.weightInput.value;
+    if (weightValue) {
+      try {
+        const request = await fetch('/.netlify/functions/addRecord', {
+          method: 'POST',
+          body: JSON.stringify({ weight: weightValue }),
+        });
+        const responseJson = await request.json();
+
+        if (!responseJson.error) {
+          this.setState(prevState => ({
+            data: [responseJson, ...prevState.data],
+          }));
+          console.log(responseJson);
         } else {
-          console.log(records);
-          this.setState({ err: handleError(records.error) });
+          console.log(responseJson);
+          this.setState({ err: handleError(responseJson.error) });
         }
-      });
+      } catch (err) {
+        console.error(err);
+      }
+
+      this.weightInput.value = '';
+    } else {
+      this.setState({ err: 'Enter a weight' });
+    }
+  }
+
+  async renderWeightRecords() {
+    try {
+      const response = await fetch('/.netlify/functions/records');
+      const recordsJson = await response.json();
+
+      if (!recordsJson.error) {
+        this.setState({ data: recordsJson });
+      } else {
+        console.log(recordsJson);
+        this.setState({ err: handleError(recordsJson.error) });
+      }
+    } catch (err) {
+      console.error(err);
+    }
   }
 
   render() {
     const { name } = this.props;
     const { data, err } = this.state;
-    if (err) {
-      return <h2>{err}</h2>;
-    }
+    console.log(data);
     return (
-      <table>
-        <caption>
-          <span role="img" aria-label="boar emoji">
-            🐗
-          </span>
-          {name}
-          <span role="img" aria-label="boar emoji">
-            🐗
-          </span>
-        </caption>
-        <thead>
-          <tr>
-            <th>Date (yyy-mm-dd)</th>
-            <th>Weight (g)</th>
-          </tr>
-        </thead>
-        {data.map(record => (
-          <tr key={record._id}>
-            <td>{record.date.substring(0, 10)}</td>
-            <td>{record.weight}</td>
-          </tr>
-        ))}
-      </table>
+      <>
+        <table>
+          <caption>
+            <span role="img" aria-label="boar emoji">
+              🐗
+            </span>
+            {name}
+            <span role="img" aria-label="boar emoji">
+              🐗
+            </span>
+          </caption>
+          <thead>
+            <tr>
+              <th>Date (yyy-mm-dd)</th>
+              <th>Weight (g)</th>
+            </tr>
+          </thead>
+          {data.map(record => (
+            <tr key={record._id}>
+              <td>{record.date.substring(0, 10)}</td>
+              <td>{record.weight}</td>
+            </tr>
+          ))}
+        </table>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="number"
+            name="weight"
+            step="100"
+            placeholder="Enter today's weight"
+            ref={input => (this.weightInput = input)}
+          />
+          <button type="submit">Save</button>
+        </form>
+        {err && <h3>{err}</h3>}
+      </>
     );
   }
 }
