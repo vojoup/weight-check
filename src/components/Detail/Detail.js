da0b2818a68bb7254d537d1e9dc2719665fc8b79
Vojta Oupicky <voj.oup@gmail.com>
2019-02-15 14:40:15 +0100

    Change document title with navigation

--- src/components/Detail/Detail.js
+++ src/components/Detail/Detail.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import Table from '../Table/Table';
 import Link from '../Link/Link';
 
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 const Detail = props => {
-  console.log(props);
+  const { name } = props.match.params;
+  useEffect(() => {
+    document.title = `${capitalizeFirstLetter(name)} weight records`;
+    return () => {
+      document.title = 'Weight check';
+    };
+  });
   return (
     <main>
       <Link id="go-back" to="/">
         Go back
       </Link>
-      <Table name={props.match.params.name} limit={30} />
+      <Table name={name} limit={30} />
     </main>
   );
 };
