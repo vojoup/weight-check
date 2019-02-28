6c4faef404f31cf3b2f81ba2a464ffbabad172ce
Vojta Oupicky <voj.oup@gmail.com>
2019-02-28 18:05:22 +0100

    Add row animation and total weight

--- src/components/PieChart/PieChart.js
+++ src/components/PieChart/PieChart.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { animated, useSpring } from 'react-spring';
 
 import ReactChartkick, { PieChart } from 'react-chartkick';
 import Chart from 'chart.js';
@@ -7,8 +8,8 @@ ReactChartkick.addAdapter(Chart);
 
 const PieChartCompare = () => {
   const [initialized, setInitialized] = useState(false);
-  const [kulikWeight, setKulikWeight] = useState(null);
-  const [montikWeight, setMontikWeight] = useState(null);
+  const [kulikWeight, setKulikWeight] = useState(0);
+  const [montikWeight, setMontikWeight] = useState(0);
 
   const getLastWeight = async (name, limit) => {
     try {
@@ -29,8 +30,11 @@ const PieChartCompare = () => {
             break;
         }
       }
-      return lastRecord.error;
+      setKulikWeight(1000);
+      setMontikWeight(1000);
     } catch (err) {
+      setKulikWeight(1000);
+      setMontikWeight(1000);
       console.error(err);
     }
   };
@@ -46,11 +50,17 @@ const PieChartCompare = () => {
   return (
     <div>
       <h2>Comparison of weights</h2>
-      <PieChart
-        colors={['#2eb47b', '#416ea8']}
-        data={{ Kulik: kulikWeight, Montik: montikWeight }}
-        suffix="g"
-      />
+      <h3>Total weight: {montikWeight + kulikWeight}g</h3>
+      <animated.div>
+        <PieChart
+          colors={['#2eb47b', '#416ea8']}
+          data={{
+            Kulik: kulikWeight,
+            Montik: montikWeight,
+          }}
+          suffix="g"
+        />
+      </animated.div>
     </div>
   );
 };
