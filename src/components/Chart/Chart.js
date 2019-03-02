6e0577d4d12981d7ccc65302f485f31cbd31f8c1
Vojta Oupicky <voj.oup@gmail.com>
2019-03-02 09:57:57 +0100

    Display point at the end and beggining of the date range

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -37,14 +37,30 @@ const getMaxinum = records => {
   return max;
 };
 
+const computeAverageWeight = records => {
+  const total = records.reduce(
+    (acc, currentValue) => acc + currentValue.weight,
+    0
+  );
+  return total / records.length;
+};
+
 const RecordsChart = ({ records, name }) => {
   const [min, setMin] = useState(0);
   const [max, setMax] = useState(0);
+  const [averageDateRange, setAverageDateRange] = useState({
+    startDate: null,
+    endDate: null,
+  });
 
   useEffect(() => {
     if (records.length) {
       setMin(getMininum(records) - 50);
       setMax(getMaxinum(records) + 50);
+      setAverageDateRange({
+        startDate: records[records.length - 1].date,
+        endDate: records[0].date,
+      });
     }
   }, [records]);
 
@@ -53,23 +69,37 @@ const RecordsChart = ({ records, name }) => {
     return obj;
   }, {});
   const color = name === 'Montik' ? '#416ea8' : '#2eb47b';
+  const average = computeAverageWeight(records);
+  const chartData = [
+    { name, data: { ...result } },
+    {
+      name: 'Average',
+      data: {
+        [averageDateRange.endDate]: average,
+        [averageDateRange.startDate]: average,
+      },
+    },
+  ];
 
   return (
-    <LineChart
-      min={min}
-      max={max}
-      xtitle="Date"
-      ytitle="Weight"
-      curve
-      id={name.toLowerCase()}
-      legend
-      colors={[color]}
-      label="Weight"
-      messages={{ empty: 'No data to be displayed' }}
-      thousands=","
-      suffix="g"
-      data={{ ...result }}
-    />
+    <>
+      {records.length && <h3>Avereage weight: {average}</h3>}
+      <LineChart
+        min={min}
+        max={max}
+        xtitle="Date"
+        ytitle="Weight"
+        curve
+        id={name.toLowerCase()}
+        legend
+        colors={[color, 'red']}
+        label="Weight"
+        messages={{ empty: 'No data to be displayed' }}
+        thousands=","
+        suffix="g"
+        data={chartData}
+      />
+    </>
   );
 };
 
